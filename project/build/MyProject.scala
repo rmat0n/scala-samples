91e56ca3d54400f46dc80d5040d8db30fd30054d
Romain MATON <romain.maton@gmail.com>
2010-05-05 16:27:41 +0200

    Update dependencies

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -5,8 +5,7 @@ class MainProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlugin
   lazy val operatorsProject = project("operators", "Operators", new OperatorsProject(_) with IdeaPlugin with Eclipsify)
   
   class OperatorsProject(info: ProjectInfo) extends DefaultProject(info) {
-    val scalatest = "org.scalatest" % "scalatest" % "1.0"
+    val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
     val scalaToolsRepo = "ScalaTools Repository" at "http://scala-tools.org/repo-releases/"
-    val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
   }
 }
